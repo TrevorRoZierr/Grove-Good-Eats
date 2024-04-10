96c73bd6c43c3b77e6688463c441d12c78aa79b9
SAAD <saadsyed950@gmail.com.com>
2024-04-10 21:45:27 +0530

    Done

--- app/components/(hamburger)/Hamburger.tsx
+++ app/components/(hamburger)/Hamburger.tsx
@@ -5,10 +5,10 @@ const Hamburger = () => {
     <div className="lg:w-[10%] w-[40%] font-berkshire tracking-wider h-[30%] bg-gradient-to-l from-lime-200 to-lime-400 text-black absolute lg:top-[15%] top-[9%] lg:left-2 left-1 flex justify-center items-center border-black border-2 rounded-lg shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)]">
       <ul className="flex flex-col">
         <li className="mb-6">
-          <Link href="">ER-Diagram</Link>
+          <Link href="/ERD.pdf" target="_blank">ER-Diagram</Link>
         </li>
         <li>
-          <Link href="">Project Owners</Link>
+          <Link href="/owners">Project Owners</Link>
         </li>
       </ul>
     </div>
---
+++ app/owners/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Owners = () => {
+  return (
+    <div className="flex justify-center items-center text-center flex-col lg:mt-24 mt-16">
+      <p className="my-5 font-unbounded font-semibold lg:text-2xl text-lg">Manager: Sarvar Ali</p>
+      <p className="my-5 font-unbounded font-semibold lg:text-2xl text-lg">Tester: Maqbool Pathan</p>
+      <p className="my-5 font-unbounded font-semibold lg:text-2xl text-lg">Architect: Shoaib Nadaf</p>
+      <p className="my-5 font-unbounded font-semibold lg:text-2xl text-lg ">Developer: Sayyed Saad</p>
+    </div>
+  );
+};
+
+export default Owners;
